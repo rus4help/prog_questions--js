3efa43ad1abef8e0b791d7a4396911431ea08453
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-08-31 17:56:10 +0300

    Реализован функционал добавления вопросов в localStorage

--- src/question.js
+++ src/question.js
@@ -9,7 +9,19 @@ export class Question {
         }) // метод fetch возвращает promise
             .then(response => response.json()) // приводит ответ к json формату
             .then(response => {
-                console.log(response)
+                question.id = response.name // создаём у объекта id и присваиваем ему name у объекта response
+                return question // возвращаем весь объект вместе с id
             })
+            .then(addToLocalStorage) // добавляем данные в локальное хранилище
     }
+}
+
+function addToLocalStorage(question) { // функция для добавления информации в локальное хранилище
+    const all = getQuestionsFromLocalStorage()              // содержит массив всех вопросов
+    all.push(question)                                      // добавляем новый вопрос в конец массива
+    localStorage.setItem('questions', JSON.stringify(all))  // обновляем локальное хранилище новым массивом вопросов
+}
+
+function getQuestionsFromLocalStorage() { // функция получения всех вопросов из локального хранилища
+    return JSON.parse(localStorage.getItem('questions') || '[]') // возвращаем все вопросы в массиве или пустой массив
 }
