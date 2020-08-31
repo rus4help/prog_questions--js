b5a0f87263a6657ea46622656c8f93ae1d2fba1b
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-08-31 15:53:42 +0300

    Логика работы с формой

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
-import './styles.css'
+import { isValid } from './utils' // имортируем функцию для проверки валидации формы
+import './styles.css'             // имортируем стили проекта
 
-console.log('KJDKJFKLJDLKJ');
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const submitBtn = form.querySelector('#submit')
+
+form.addEventListener('submit', submitFormHandler)
+input.addEventListener('input', () => {
+    submitBtn.disabled = !isValid(input.value) // отключаем кнопку, если форма не валидна
+})
+
+function submitFormHandler(event) {
+    event.preventDefault() // отменяем действие кнопки по умолчанию
+
+    if (isValid(input.value)) { // проверяем, если валидация формы прошла,
+        const question = {      // создаём объект вопроса
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+
+        submitBtn.disabled = true // блокируем кнопку для предотвращения спама отправки данных до завершения самой отправки
+        // После получение данных нужно асинхронно отправить данные на сервер для сохранения
+        console.log('Question', question)
+
+        input.value = ''        // после завершения отправки запроса очищаем форму
+        input.className = ''    // очищаем ненужные классы у поля
+        submitBtn.disabled = false  // делаем кнопку активной 
+    }
+}
